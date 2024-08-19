03be771c4bd7f8e360ae736663215f5b8a7ddedc
Doc-UP-AlejandroJaimes <juan.carrilloju@unipamplona.edu.co>
2024-08-19 16:42:29 -0500

    feat(linked-list): remove, reverse methods

--- content/Section03_LinkedLists/SC03_linked_list.py
+++ content/Section03_LinkedLists/SC03_linked_list.py
@@ -132,55 +132,40 @@ class LinkedList:
         self.length += 1
         return True
     
-    def remove(self, index, value) -> bool:
-        pass
-    
+    def remove(self, index) -> bool:
+        # check that is on the limits
+        if index < 0 or index >= self.length:
+            return None
+        #check if the node seaking is the first
+        if index == 0:
+            return self.pop_first()
+        # check if the node seaking is the last
+        if index == self.length - 1:
+            return self.pop()
+        # remove in the middle
+        previous_last_node_to_remove = self.get(index - 1)
+        last_node_to_remove = self.get(index)
+        previous_last_node_to_remove.next = last_node_to_remove.next
+        last_node_to_remove.next = None
+        self.length -= 1
+        return last_node_to_remove
+        
+    def reverse(self) -> None:
+        # switch head and tail
+        tmp_head = self.head
+        self.head = self.tail
+        self.tail = tmp_head
+        # variables to switch the elements
+        after = tmp_head.next
+        before = None
+        for _ in range(self.length):
+            after = tmp_head.next
+            tmp_head.next = before
+            before = tmp_head
+            tmp_head = after
+        
     def print_list(self) -> None:
         tmp = self.head
         while tmp is not None:
             print(tmp.value, end=" ")
             tmp = tmp.next
-    
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-#my_linked_list.append(3)
-#my_linked_list.append(4)
-#print(my_linked_list.length)
-my_linked_list.print_list()
-print(f"\nHead: {my_linked_list.head.value} Tail: {my_linked_list.tail.value}")
-
-my_linked_list.prepend(11)
-my_linked_list.prepend(10)
-my_linked_list.prepend(8)
-my_linked_list.print_list()
-print(f"\nHead: {my_linked_list.head.value} Tail: {my_linked_list.tail.value}")
-
-# my_linked_list.pop_first()
-# my_linked_list.pop_first()
-# my_linked_list.pop_first()
-# my_linked_list.pop_first()
-
-print(my_linked_list.get(2))
-print(my_linked_list.get(-2))
-print(my_linked_list.get(4))
-print(my_linked_list.get(3))
-print(my_linked_list.set_value(0,19))
-print(my_linked_list.set_value(4,85))
-
-
-#print(my_linked_list.get2(2))
-#print(my_linked_list.get2(0))
-
-my_linked_list.print_list()
-print(f"\nHead: {my_linked_list.head.value} Tail: {my_linked_list.tail.value}")
-
-print(my_linked_list.insert(3,55))
-print(my_linked_list.insert(2,54))
-print(my_linked_list.insert(7,44))
-my_linked_list.print_list()
-print(f"\nHead: {my_linked_list.head.value} Tail: {my_linked_list.tail.value}")
-
-# print(my_linked_list.pop()) # 2 
-# print(my_linked_list.pop()) # 1
-# print(my_linked_list.pop()) # 0
-#print(f"\nHead: {my_linked_list.head.value} Tail: {my_linked_list.tail.value}")
